a078f067498737ec351aa4592ef207149fc9646b
Kamil Maciejczyk <kamil.maciejczyk@accenture.com>
2023-06-13 09:05:42 +0200

    basic functionality

--- app/src/main/java/com/example/weathermaster/ForecastAdapter.java
+++ app/src/main/java/com/example/weathermaster/ForecastAdapter.java
@@ -32,6 +32,12 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Foreca
         holder.dateText.setText(forecast.getDate());
         holder.temperatureText.setText(forecast.getTemperature());
         holder.weatherDescriptionText.setText(forecast.getWeatherDescription());
+        holder.humidityText.setText(forecast.getHumidity());
+        holder.windspeedText.setText(forecast.getWindspeed());
+        holder.pressureText.setText(forecast.getPressure());
+        holder.visibilityText.setText(forecast.getVisibility());
+        holder.winddirectionText.setText(forecast.getWinddirection());
+        holder.conditionsText.setText(forecast.getConditions());
     }
 
     @Override
@@ -43,12 +49,24 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Foreca
         public TextView dateText;
         public TextView temperatureText;
         public TextView weatherDescriptionText;
+        public TextView humidityText;
+        public TextView windspeedText;
+        public TextView pressureText;
+        public TextView visibilityText;
+        public TextView winddirectionText;
+        public TextView conditionsText;
 
         public ForecastViewHolder(View view) {
             super(view);
             dateText = view.findViewById(R.id.date_text);
             temperatureText = view.findViewById(R.id.forecast_temperature_text);
             weatherDescriptionText = view.findViewById(R.id.forecast_weather_description_text);
+            humidityText = view.findViewById(R.id.humidity_text);
+            windspeedText = view.findViewById(R.id.windspeed_text);
+            pressureText = view.findViewById(R.id.pressure_text);
+            visibilityText = view.findViewById(R.id.visibility_text);
+            winddirectionText = view.findViewById(R.id.winddirection_text);
+            conditionsText = view.findViewById(R.id.conditions_text);
         }
     }
 }
--- app/src/main/java/com/example/weathermaster/WeatherForecast.java
+++ app/src/main/java/com/example/weathermaster/WeatherForecast.java
@@ -4,11 +4,23 @@ public class WeatherForecast {
     private final String date;
     private final String temperature;
     private final String weatherDescription;
+    private final String humidity;
+    private final String windspeed;
+    private final String pressure;
+    private final String visibility;
+    private final String winddirection;
+    private final String conditions;
 
-    public WeatherForecast(String date, String temperature, String weatherDescription) {
+    public WeatherForecast(String date, String temperature, String weatherDescription, String humidity, String windspeed, String pressure, String visibility,String winddirection, String conditions) {
         this.date = date;
         this.temperature = temperature;
         this.weatherDescription = weatherDescription;
+        this.humidity = humidity;
+        this.windspeed = windspeed;
+        this.pressure = pressure;
+        this.visibility = visibility;
+        this.winddirection = winddirection;
+        this.conditions = conditions;
     }
 
     public String getDate() {
@@ -22,5 +34,11 @@ public class WeatherForecast {
     public String getWeatherDescription() {
         return weatherDescription;
     }
+    public String getHumidity() { return humidity;   }
+    public String getWindspeed() { return windspeed;   }
+    public String getPressure() { return pressure;   }
+    public String getVisibility() { return visibility;   }
+    public String getWinddirection() { return winddirection;   }
+    public String getConditions() { return conditions;   }
 }
 
