2b5f228e1548c8c23fe890fe334a6b927a7f5d9a
patchker <patchker@icloud.com>
2023-06-19 17:29:58 +0200

    ulepszony wyglad i dzialanie

--- app/src/main/java/com/example/weathermaster/HourlyForecastActivity.java
+++ app/src/main/java/com/example/weathermaster/HourlyForecastActivity.java
@@ -8,6 +8,8 @@ import android.view.MenuItem;
 import android.widget.Button;
 
 import androidx.recyclerview.widget.RecyclerView;
+
+import java.time.LocalDate;
 import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -20,20 +22,30 @@ public class HourlyForecastActivity extends AppCompatActivity implements Weather
     private Executor executor;
     private ForecastAdapter forecastAdapter;
 
+    private String selectedCity; // Dodaj zmienną przechowującą wybrane miasto
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hourly_forecast);
         SSLUtils.disableCertificateValidation();
+        if (getIntent().hasExtra("miasto")) {
+            selectedCity = getIntent().getStringExtra("miasto");
+        } else {
+            selectedCity = "London"; // Domyślne miasto, jeśli nie zostało wybrane żadne
+        }
 
         hourlyForecastRecyclerView = findViewById(R.id.hourly_forecast_recycler_view);
         weatherHourlyRequest = new WeatherHourlyRequest(this);
         executor = Executors.newSingleThreadExecutor();
-        executeWeatherRequest("Gliwice");
+        executeWeatherRequest(selectedCity);
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
+
+
+
 
     }
     @Override
@@ -47,8 +59,8 @@ public class HourlyForecastActivity extends AppCompatActivity implements Weather
     }
 
     private void executeWeatherRequest(String city) {
-
-        String hourlyForecastUrl = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + city +"/2023-06-13/2023-06-13?unitGroup=metric&elements=datetime%2Ctemp%2Chumidity%2Cwindspeed%2Cwinddir%2Cpressure%2Cvisibility%2Cconditions%2Cdescription&include=hours&key=5JFQTUSUAU529CC89JAB3XYS4&contentType=json";
+        LocalDate startDate = LocalDate.now();
+        String hourlyForecastUrl = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/"+ city + "/"+startDate.toString()+"/"+startDate.toString()+"?unitGroup=metric&elements=datetime%2Ctemp%2Chumidity%2Cwindspeed%2Cwinddir%2Cpressure%2Cvisibility%2Cconditions%2Cdescription&include=hours&key=5JFQTUSUAU529CC89JAB3XYS4&contentType=json";
         executor.execute(() -> weatherHourlyRequest.executeRequest(hourlyForecastUrl));
     }
 
@@ -56,7 +68,7 @@ public class HourlyForecastActivity extends AppCompatActivity implements Weather
     @Override
     public void onHourlyRequestCompleted(List<WeatherForecast> forecastList) {
         runOnUiThread(() -> {
-            hourlyForecastAdapter = new HourlyForecastAdapter(forecastList);
+            hourlyForecastAdapter = new HourlyForecastAdapter(forecastList, selectedCity);
             hourlyForecastRecyclerView.setAdapter(hourlyForecastAdapter);
         });
     }
--- app/src/main/java/com/example/weathermaster/MainActivity.java
+++ app/src/main/java/com/example/weathermaster/MainActivity.java
@@ -1,11 +1,13 @@
 package com.example.weathermaster;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
 
-import androidx.recyclerview.widget.RecyclerView;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -15,6 +17,8 @@ public class MainActivity extends AppCompatActivity implements WeatherRequest.We
     private WeatherRequest weatherRequest;
     private Executor executor;
     private ForecastAdapter forecastAdapter;
+
+    private String selectedCity; // Dodaj zmienną przechowującą wybrane miasto
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,34 +37,45 @@ public class MainActivity extends AppCompatActivity implements WeatherRequest.We
 
         hourlyForecastButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, HourlyForecastActivity.class);
+            intent.putExtra("miasto", selectedCity); // Dodaj wybrane miasto do Intentu
             startActivity(intent);
         });
 
+
         sevenDaysForecastButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, SevenDaysForecastActivity.class);
+            intent.putExtra("miasto", selectedCity); // Dodaj wybrane miasto do Intentu
             startActivity(intent);
         });
 
         mapButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, Map.class);
+            intent.putExtra("miasto", selectedCity); // Dodaj wybrane miasto do Intentu
             startActivity(intent);
         });
-        executeWeatherRequest("Gliwice");
+
+
+        // Sprawdź, czy Intent zawiera wybrane miasto
+        if (getIntent().hasExtra("miasto")) {
+            selectedCity = getIntent().getStringExtra("miasto");
+        } else {
+            selectedCity = "London"; // Domyślne miasto, jeśli nie zostało wybrane żadne
+        }
+
+        executeWeatherRequest(selectedCity);
     }
 
     private void executeWeatherRequest(String city) {
-        String dailyForecastUrl = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + city +"/2023-06-13/2023-06-13?unitGroup=metric&elements=datetime%2Ctemp%2Chumidity%2Cwindspeed%2Cwinddir%2Cpressure%2Cvisibility%2Cconditions%2Cdescription&include=days&key=5JFQTUSUAU529CC89JAB3XYS4&contentType=json";
+        LocalDate startDate = LocalDate.now();
+        String dailyForecastUrl = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + city + "/"+startDate.toString()+"/"+startDate.toString()+"?unitGroup=metric&elements=datetime%2Ctemp%2Chumidity%2Cwindspeed%2Cwinddir%2Cpressure%2Cvisibility%2Cconditions%2Cdescription&include=days&key=5JFQTUSUAU529CC89JAB3XYS4&contentType=json";
         executor.execute(() -> weatherRequest.executeRequest(dailyForecastUrl));
-
     }
 
     @Override
     public void onRequestCompleted(List<WeatherForecast> forecastList) {
         runOnUiThread(() -> {
-            forecastAdapter = new ForecastAdapter(forecastList);
+            forecastAdapter = new ForecastAdapter(forecastList, selectedCity);
             forecastRecyclerView.setAdapter(forecastAdapter);
         });
     }
-
-
 }
