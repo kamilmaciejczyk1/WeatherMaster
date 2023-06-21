899ca2ada3b93d258f6eea25f2d123df1d3e8ad9
KeRoN00 <listwon.dev@gmail.com>
2023-06-21 18:25:57 +0200

    Przyciski, zmiany miast

--- app/src/main/java/com/example/weathermaster/MainActivity.java
+++ app/src/main/java/com/example/weathermaster/MainActivity.java
@@ -6,10 +6,15 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.SearchView;
+import android.widget.Spinner;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
@@ -36,21 +41,52 @@ public class MainActivity extends AppCompatActivity implements WeatherRequest.We
         Button hourlyForecastButton = findViewById(R.id.hourly_forecast_button);
         Button sevenDaysForecastButton = findViewById(R.id.seven_days_forecast_button);
         Button mapButton = findViewById(R.id.map_button);
-        Button addLocationButton = findViewById(R.id.add_location_button);
-        final SearchView searchbar = findViewById(R.id.searchbar);
+        Button searchButton = findViewById(R.id.search_button);
+        Button addButton = findViewById(R.id.add_location_button);
+        SearchView searchbar = findViewById(R.id.searchbar);
+        int closeButtonId = searchbar.getContext().getResources()
+                .getIdentifier("android:id/search_close_btn", null, null);
+        ImageView clearSearchBar = findViewById(closeButtonId);
+        Spinner citySpinner = findViewById(R.id.citySpinner);
+
+// Przygotowanie danych miast
+        List<String> cityList = new ArrayList<String>();
+        cityList.add("Katowice");
+        cityList.add("Gliwice");
+
+// Utwórz adapter i przypisz go do Spinera
+        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, cityList);
+        spinnerAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        citySpinner.setAdapter(spinnerAdapter);
+
+        citySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String selectedCityItem = (String) parent.getItemAtPosition(position);
+                selectedCity = selectedCityItem;
+                executeWeatherRequest(selectedCity);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Obsłuż sytuację, gdy nie jest wybrane żadne miasto
+            }
+        });
+
         hourlyForecastButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, HourlyForecastActivity.class);
             intent.putExtra("miasto", selectedCity); // Dodaj wybrane miasto do Intentu
             startActivity(intent);
         });
 
-        addLocationButton.setOnClickListener(v -> {
+        searchButton.setOnClickListener(v -> {
 
             if (searchbar.getVisibility() == View.INVISIBLE) {
                 searchbar.setVisibility(View.VISIBLE);
-                searchbar.setBackgroundColor(getColor(R.color.white));
+                searchbar.setBackgroundColor(getColor(R.color.lightgray));
             } else {
                 // Dodaj lokalizację do globalnej listy miejsc jeśli istnieje
+
                 // Albo zrobić nowy ekran z wyszukanym miastem albo od razu szukać po wpisaniu
 
                 // Wyzeruj pole wyszukiwania
@@ -60,7 +96,45 @@ public class MainActivity extends AppCompatActivity implements WeatherRequest.We
                 searchbar.setVisibility(View.INVISIBLE);
             }
         });
+        addButton.setOnClickListener(v -> {
 
+        });
+        clearSearchBar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchbar.setQuery("", false);  // Wyczyść wprowadzoną wartość
+                searchbar.clearFocus();        // Zabierz fokus z pola wyszukiwania
+                searchbar.setVisibility(View.INVISIBLE); // Ustaw widoczność na INVISIBLE
+            }
+        });
+        searchbar.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+                                             @Override
+                                             public boolean onQueryTextSubmit(String query) {
+                                                 String searchQuery = searchbar.getQuery().toString();
+                                                 if(searchQuery.isEmpty()){
+                                                     searchbar.setVisibility(View.INVISIBLE);
+                                                     return false;
+                                                 }
+                                                 selectedCity = searchQuery;
+                                                 executeWeatherRequest(searchQuery);
+
+                                                 searchbar.setVisibility(View.INVISIBLE);
+                                                 searchbar.setQuery("", false);
+                                                 return false;
+                                             }
+
+                                             @Override
+                                             public boolean onQueryTextChange(String newText) {
+                                                 return false;
+                                             }
+                                         });
+
+        searchbar.setOnCloseListener(new SearchView.OnCloseListener() {
+            @Override
+            public boolean onClose() {
+                return false;
+            }
+        });
         sevenDaysForecastButton.setOnClickListener(v -> {
             Intent intent = new Intent(MainActivity.this, SevenDaysForecastActivity.class);
             intent.putExtra("miasto", selectedCity); // Dodaj wybrane miasto do Intentu
@@ -78,8 +152,8 @@ public class MainActivity extends AppCompatActivity implements WeatherRequest.We
         if (getIntent().hasExtra("miasto")) {
             selectedCity = getIntent().getStringExtra("miasto");
         } else {
-            selectedCity = "London"; // Domyślne miasto, jeśli nie zostało wybrane żadne
-        }
+                selectedCity = "Katowice";
+            }
 
         executeWeatherRequest(selectedCity);
     }
